d245114baa42457d2100ec707adab14215f8f458
noriatsu yoshimoto <sukkyxp@yahoo.co.jp>
2019-11-04 10:37:28 +0900

    メソッド分離

--- MyJavaDir/MyJavaApp/src/TestApp.java
+++ MyJavaDir/MyJavaApp/src/TestApp.java
@@ -1,10 +1,11 @@
-
 public class TestApp {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.print("Hello Wrold!");
+		printGreet();
+	}
 
+	private static void printGreet() {
+		System.out.print("Hello Wrold!");
 	}
 
 }
