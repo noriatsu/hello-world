92bfe52f4f772b67bab43d380b0f7cb7a636017c
noriatsu yoshimoto <sukkyxp@yahoo.co.jp>
2019-11-04 23:10:02 +0900

    関数コメント追加

--- MyJavaDir/MyJavaApp/src/com/aoisonori/test/greet/TestApp.java
+++ MyJavaDir/MyJavaApp/src/com/aoisonori/test/greet/TestApp.java
@@ -2,10 +2,18 @@ package com.aoisonori.test.greet;
 
 public class TestApp {
 
+	/***
+	 * メイン関数
+	 *
+	 * @param args 引数
+	 */
 	public static void main(String[] args) {
 		printGreet();
 	}
 
+	/***
+	 * 挨拶表示関数
+	 */
 	private static void printGreet() {
 		System.out.print("Hello Wrold!");
 	}
